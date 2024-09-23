ead13dae826a08ef43024cc3069594b100a43cf8
Calvin <calvinsidik@gmail.com>
2024-09-24 00:00:10 +0700

    home page

--- src/components/Modules/InputBox/index.jsx
+++ src/components/Modules/InputBox/index.jsx
@@ -3,10 +3,13 @@ import { GoSearch } from 'react-icons/go'
 
 const InputBox = () => {
     return (
-        <div className='relative'>
-            <input className='text-stone-500 bg-white rounded-full pl-12'></input>
-            <GoSearch fill='black' className='font-bold text-red absolute top-1/2 -translate-y-1/2 z-10 left-3' />
+        <div>
+            <div className='relative ml-4'>
+                <input className='text-stone-500 bg-white rounded-full pl-12'></input>
+                <GoSearch fill='black' className='font-bold text-red absolute top-1/2 -translate-y-1/2 z-10 left-3' />
+            </div>
         </div>
+
     )
 }
 
--- src/pages/Home/Navbar.jsx
+++ src/pages/Home/Navbar.jsx
@@ -5,14 +5,18 @@ const Navbar = () => {
     return (
         <header className='relative z-20'>
             <nav className='flex flex-wrap items-center'>
-
                 <div>
                     <img src='netflix-logo-icon.png' width={200} height={100}></img>
                 </div>
-                <div className='flex p-8 gap-10'>
-                    <h3>Product</h3>
-                    <h3>Games</h3>
+                <div className='flex p-8 gap-2'>
+                    <button className='hover:bg-white/30 rounded pl-3 pr-3'>Product</button>
+                    <button className='hover:bg-white/30 rounded pl-3 pr-3'>Games</button>
                     <InputBox />
+                    <div className='avatar absolute right-0'>
+                        <div className="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2 mr-8">
+                            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        </div>
+                    </div>
                 </div>
 
             </nav>
